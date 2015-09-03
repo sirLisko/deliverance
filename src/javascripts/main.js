135d725a50c2f59e3bf686f62edf330064decd3a
Luca Lischetti <sirlisko@gmail.com>
2015-09-03 12:49:10 +0100

    added config and analytics

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,7 @@
 import angular from 'angular'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import '../stylesheets/main.css'
 
 import menuController from './modules/menu.controller'
 import menuService from './modules/menu.service'
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
+var path = require('path')
+
 module.exports = {
   devtool: 'source-map',
-  entry: './src/javascripts/main',
+  context: path.resolve(__dirname, 'src/javascripts'),
+  entry: './main',
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      exclude: /node_modules/
-    }]
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+    ]
   },
   output: {
     filename: 'public/js/main.js'
