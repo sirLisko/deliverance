75de8b4e90f2a10a139648e563a870877237680c
Luca Lischetti <luca@sirlisko.com>
2016-07-10 09:14:25 +0100

    linted with standard syntax

--- public/js/main.js
+++ public/js/main.js
@@ -1,94 +1,88 @@
 /*global angular, localStorage*/
 
-(function(){
-	'use strict';
+angular.module('deliverance', [ ])
+.controller('DeliveranceController', function ($http, $scope) {
+  var lastOrder
 
-	var app = angular.module('deliverance', [ ]);
+  $scope.dishes = []
 
-	app.controller('DeliveranceController', ['$http', '$scope', function($http, $scope){
-    	var lastOrder;
+  $http.get('/v1/menu').success(function (data) {
+    $scope.dishes = data
+  })
 
-		$scope.dishes = [];
+  function calculateTotals (price) {
+    $scope.maxPrice = parseFloat($scope.maxPrice)
+    $scope.orderTotal += price
+    $scope.maxPrice -= price
+    $scope.maxPrice = $scope.maxPrice.toFixed(2)
+  }
 
-		$http.get('/v1/menu').success(function(data){
-			$scope.dishes = data;
-		});
+  $scope.clearOrder = function () {
+    $scope.maxPrice = (15).toFixed(2)
+    $scope.order = []
+    $scope.orderTotal = 0
+  }
 
-		function calculateTotals (price) {
-			$scope.maxPrice = parseFloat($scope.maxPrice);
-			$scope.orderTotal += price;
-			$scope.maxPrice -= price;
-			$scope.maxPrice = $scope.maxPrice.toFixed(2);
-		}
+  $scope.saveOrder = function () {
+    localStorage.setItem('lastOrder', angular.toJson({
+      'maxPrice': $scope.maxPrice,
+      'orderTotal': $scope.orderTotal,
+      'order': $scope.order
+    }))
+  }
 
-		$scope.clearOrder = function(){
-			$scope.maxPrice = (15).toFixed(2);
-			$scope.order = [];
-			$scope.orderTotal = 0;
-		};
+  $scope.addToOrder = function (dish) {
+    calculateTotals(dish.price)
+    $scope.order.push(angular.copy(dish))
+    if (dish.extra) {
+      dish.extra.forEach(function (el) {
+        el.selected = false
+      })
+    }
+  }
 
-		$scope.saveOrder = function(){
-			localStorage.setItem('lastOrder', JSON.stringify({
-				'maxPrice': $scope.maxPrice,
-				'orderTotal': $scope.orderTotal,
-				'order': $scope.order
-			}));
-		};
+  $scope.removeFromOrder = function (dish) {
+    calculateTotals(-dish.price)
+    var index = $scope.order.indexOf(dish)
+    $scope.order.splice(index, 1)
+  }
 
-		$scope.addToOrder = function(dish){
-			calculateTotals(dish.price);
-			$scope.order.push(angular.copy(dish));
-			if (dish.extra) {
-				dish.extra.forEach(function(el){
-					el.selected = false;
-				});
-			}
-		};
+  $scope.addExtra = function (extra, dish, e, order) {
+    e.stopPropagation()
+    if (extra.selected) {
+      extra.selected = false
+      dish.price -= extra.price
+      if (order) {
+        calculateTotals(-extra.price)
+      }
+    } else {
+      if ((order ? 0 : dish.price) + $scope.orderTotal + extra.price > 15) { return false }
+      extra.selected = true
+      dish.price += extra.price
+      if (order) {
+        calculateTotals(extra.price)
+      }
+    }
+  }
 
-		$scope.removeFromOrder = function(dish){
-			calculateTotals(-dish.price);
-			var index = $scope.order.indexOf(dish);
-			$scope.order.splice(index, 1);
-		};
+  if ((lastOrder = localStorage.getItem('lastOrder'))) {
+    lastOrder = angular.fromJson(lastOrder)
+    $scope.maxPrice = lastOrder.maxPrice
+    $scope.orderTotal = lastOrder.orderTotal
+    $scope.order = lastOrder.order
+  } else {
+    $scope.clearOrder()
+  }
+})
 
-		$scope.addExtra = function(extra, dish, e, order){
-			e.stopPropagation();
-			if (extra.selected) {
-				extra.selected = false;
-				dish.price -= extra.price;
-				if (order) {
-					calculateTotals(-extra.price);
-				}
-			} else {
-				if ((order ? 0 : dish.price) + $scope.orderTotal + extra.price > 15) { return false; }
-				extra.selected = true;
-				dish.price += extra.price;
-				if (order) {
-					calculateTotals(extra.price);
-				}
-			}
-		};
-
-		if(lastOrder = localStorage.getItem('lastOrder')) {
-			lastOrder = JSON.parse(lastOrder);
-			$scope.maxPrice = lastOrder.maxPrice;
-			$scope.orderTotal = lastOrder.orderTotal;
-			$scope.order = lastOrder.order;
-		} else {
-			$scope.clearOrder();
-		}
-	}]);
-
-	app.filter('lt', function () {
-		return function ( items, value ) {
-			var filteredItems = [];
-			angular.forEach(items, function ( item ) {
-				if ( item.price <= value ) {
-					filteredItems.push(item);
-				}
-			});
-			return filteredItems;
-		};
-	});
-
-})();
+.filter('lt', function () {
+  return function (items, value) {
+    var filteredItems = []
+    angular.forEach(items, function (item) {
+      if (item.price <= value) {
+        filteredItems.push(item)
+      }
+    })
+    return filteredItems
+  }
+})
