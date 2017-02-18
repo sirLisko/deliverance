63e8460bd4608b43f1412b228cc112485c49d90b
Luca Lischetti <luca@sirlisko.com>
2017-02-18 14:41:31 +0000

    linted app.js

--- app.js
+++ app.js
@@ -1,48 +1,44 @@
-'use strict';
+var deliverance = require('deliverance-scraper')
+var express = require('express')
+var path = require('path')
+var fs = require('fs')
 
-var deliverance = require('deliverance-scraper');
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
-
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+var app = express()
+app.use(express.static(path.join(__dirname, 'public')))
 
 // deliverance.co.uk is not longer available
-var menuCached = require('./menuCached.json').menuCached;
+var menuCached = require('./menuCached.json').menuCached
 
 if (fs.existsSync('./config.json')) {
-	var config = require('./config.json');
-	if (config.exclude) {
-		deliverance.exclude(config.exclude);
-	}
+  var config = require('./config.json')
+  if (config.exclude) {
+    deliverance.exclude(config.exclude)
+  }
 }
 
-app.get('/v1/menu', function(req, res){
-	if(menuCached) {
-		res.send(menuCached);
-	} else {
-		deliverance.getMenu().then(function(menu){
-			res.send(menu);
-			menuCached = menu;
-		}).done();
-	}
-});
+app.get('/v1/menu', (req, res) => {
+  if (menuCached) {
+    res.send(menuCached)
+  } else {
+    deliverance.getMenu().then((menu) => {
+      res.send(menu)
+      menuCached = menu
+    }).done()
+  }
+})
 
-app.get('/v1/menu/:specificMenu', function(req, res){
-	var specificMenu = req.params.specificMenu;
-	deliverance.getMenu(specificMenu).then(function(menu){
-		res.send(menu);
-	}).done();
-});
+app.get('/v1/menu/:specificMenu', (req, res) => {
+  var specificMenu = req.params.specificMenu
+  deliverance.getMenu(specificMenu).then((menu) => {
+    res.send(menu)
+  }).done()
+})
 
-app.get('/v1/menuList', function(req, res){
-	deliverance.getMenuList().then(function(list){
-		res.send(list);
-	}).done();
-});
+app.get('/v1/menuList', (req, res) => {
+  deliverance.getMenuList().then((list) => {
+    res.send(list)
+  }).done()
+})
 
-var port = process.env.PORT || 3000;
-app.listen(port);
-
-console.log('Listening on port ' + port + '...');
+var port = process.env.PORT || 3000
+app.listen(port)
